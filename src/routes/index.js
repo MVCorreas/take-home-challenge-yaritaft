73e5808ac5201dfb55838bcc1b4f92ebb4e263c9
MVCorreas <correasmv@gmail.com>
2025-12-13 10:21:43 -0300

    feat(middleware): added authentication middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import authRouter from "./authRouter";
 import notificationRouter from "./notificationRouter";
+import authMiddleware from "../middlewares/authMiddleware";
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
-router.use("/notifications", notificationRouter);
+router.use("/notifications", authMiddleware, notificationRouter);
 
 export default router;
