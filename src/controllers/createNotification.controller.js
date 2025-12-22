41317bad31df3bfdd68a654c68c7a519aaf891ee
MVCorreas <correasmv@gmail.com>
2025-12-22 19:59:23 -0300

    feat: added basic stub for createNotification endpoint

---
+++ src/controllers/createNotification.controller.js
@@ -0,0 +1,3 @@
+export const createNotification = async (req, res, next) => {
+    res.send("notification create endpoint")
+}
---
+++ src/routes/notificationRouter.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createNotification } from "../controllers/createNotification.controller.js";
+
+const router = express.Router();
+
+router.post("/", createNotification);
+// router.put("/:id", updateNotification);
+// router.delete("/:id", deleteNotification);
+// router.get("/", getNotifications)
+
+export default router;
