7567efe2aea10c26ea672db69f1c2aa6e40bd0bb
MVCorreas <correasmv@gmail.com>
2025-12-26 17:04:50 -0300

    test: added error validation test for register user

--- tests/test-suites/auth-tests/registerUser.test.js
+++ tests/test-suites/auth-tests/registerUser.test.js
@@ -5,7 +5,7 @@ import { cleanDatabase, closeDatabase } from '../../helpers/prisma-db-setup.js';
 describe('POST /auth/register', () => {
   beforeEach(async () => {
     await cleanDatabase();
-  });
+  }); 
 
   afterAll(async () => {
     await closeDatabase();
@@ -20,23 +20,22 @@ describe('Successful response', () => {
       });
 
     expect(res.statusCode).toBe(201);
-    //expect(res.body).toHaveProperty('message', 'User registered correctly');
-   // expect(res.body.user).toHaveProperty('email', 'yamila@mail.com');
+    expect(res.body).toHaveProperty('message', 'User registered correctly');
+    expect(res.body.user).toHaveProperty('email', 'yamila@mail.com');
   });
 
 });
 describe('Error response', () => {
-  it('should register a new user and return success response', async () => {
+  it('should throw ValidationError if fields are missing', async () => {
     const res = await request(app)
       .post('/auth/register')
       .send({
-        email: 'yamila@mail.com',
+        email: '',
         password: '123456',
       });
 
-    expect(res.statusCode).toBe(201);
-    //expect(res.body).toHaveProperty('message', 'User registered correctly');
-   // expect(res.body.user).toHaveProperty('email', 'yamila@mail.com');
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty('error', 'Email and password are required');
   });
 
 })
