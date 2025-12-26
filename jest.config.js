2b28cb0aeb6a614cbeaecc2dd4a33068db829122
MVCorreas <correasmv@gmail.com>
2025-12-26 10:25:55 -0300

    feat: installed and set up jest with a separate postgres db

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+export default {
+  testEnvironment: 'node',
+  transform: {},
+  moduleFileExtensions: ['js'],
+  testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/server.js',
+    '!src/generated/**',
+    '!**/node_modules/**'
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
